34e7450c29eb05a07b97d0ea40274fba9e51b7d6
Konrad Kreczko <konradkreczko@gmail.com>
2025-01-07 14:59:03 +0100

    feat: Added more packet handling

--- sniffer/src/main.c
+++ sniffer/src/main.c
@@ -16,6 +16,64 @@ pcap_handler packet_handler;
 //TODO extrude this methods to their own headers and source files
 //TODO implement this methods
 void stop_capture(int sig_number);
+void packet_handler(u_char *user, const struct pcap_pkthdr *packethdr, const u_char *packetptr) {
+    struct *ip iphdr;
+    struct *icmp icmphdr;
+    struct *tcphdr tcphdr;
+    struct *udphdr udphdr;
+
+    char ipdhrInfo[256];
+    char srcip[256];
+    char dstip[256];
+
+    packetptr += linkhdrlen;
+    iphdr = (struct ip*)packetptr;
+    strcpy(srcip, inet_ntoa(iphdr->ip_src));
+    strcpy(dstip, inet_ntoa(iphdr->ip_dst));
+    sprintf(iphdrInfo, "ID:%d TOS:0x%x, TTL:%d IpLen:%d DgLen:%d",
+            ntohs(iphdr->ip_id), iphdr->ip_tos, iphdr->ip_ttl,
+            4*iphdr->ip_hl, ntohs(iphdr->ip_len));
+    
+    packetptr += 4*iphdr->ip_hl;
+    switch (iphdr->ip_p) {
+        case IPPROTO_TCP:
+            tcphdr = (struct tcphdr*)packetptr;
+            printf("TCP  %s:%d -> %s:%d\n", srcip, ntohs(tcphdr->th_sport),
+                   dstip, ntohs(tcphdr->th_dport));
+            printf("%s\n", iphdrInfo);
+            printf("%c%c%c%c%c%c Seq: 0x%x Ack: 0x%x Win: 0x%x TcpLen: %d\n",
+                   (tcphdr->th_flags & TH_URG ? 'U' : '*'),
+                   (tcphdr->th_flags & TH_ACK ? 'A' : '*'),
+                   (tcphdr->th_flags & TH_PUSH ? 'P' : '*'),
+                   (tcphdr->th_flags & TH_RST ? 'R' : '*'),
+                   (tcphdr->th_flags & TH_SYN ? 'S' : '*'),
+                   (tcphdr->th_flags & TH_SYN ? 'F' : '*'),
+                   ntohl(tcphdr->th_seq), ntohl(tcphdr->th_ack),
+                   ntohs(tcphdr->th_win), 4*tcphdr->th_off);
+            printf("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n\n");
+            packets += 1;
+            break;
+
+        case IPPROTO_UDP:
+            udphdr = (struct udphdr*)packetptr;
+            printf("UDP  %s:%d -> %s:%d\n", srcip, ntohs(udphdr->uh_sport),
+                   dstip, ntohs(udphdr->uh_dport));
+            printf("%s\n", iphdrInfo);
+            printf("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n\n");
+            packets += 1;
+            break;
+ 
+        case IPPROTO_ICMP:
+            icmphdr = (struct icmp*)packetptr;
+            printf("ICMP %s -> %s\n", srcip, dstip);
+            printf("%s\n", iphdrInfo);
+            printf("Type:%d Code:%d ID:%d Seq:%d\n", icmphdr->icmp_type, icmphdr->icmp_code,
+                   ntohs(icmphdr->icmp_hun.ih_idseq.icd_id), ntohs(icmphdr->icmp_hun.ih_idseq.icd_seq));
+            printf("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n\n");
+            packets += 1;
+            break;
+        }
+    }
 
 int get_link_header_len(pcap_t *handle) {
     int linktype;
@@ -40,7 +98,7 @@ int get_link_header_len(pcap_t *handle) {
             printf("Unsupported data link (%d)\n", linktype);
             linkhdrlen = 0;
     }
-};
+}
 
 pcap_t* create_pcap_handle(char* device, char* filter) {
     char errbuf[PCAP_ERRBUF_SIZE];
