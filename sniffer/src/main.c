48d5be4c96dc51d9e3970231099bdacfd7c4263e
Konrad Kreczko <konradkreczko@gmail.com>
2025-01-08 21:45:51 +0100

    fix: Small rework, now everything is working

--- sniffer/src/main.c
+++ sniffer/src/main.c
@@ -7,46 +7,56 @@
 #include <netinet/tcp.h>
 #include <netinet/udp.h>
 #include <netinet/ip_icmp.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
 
-pcap_t* handle;
-int linkhdrlen;
-int packets;
+pcap_t* handle = NULL;
+int linkhdrlen = 0;
+int packets = 0;
 
-//ERROR EVERYTHING NEEDS DEBUGGING
-//TODO rework code for readability
-//TODO extrude this methods to their own headers and source files
-void stop_capture(int sig_number){
+//TODO move methods to their own header and source files
+void stop_capture(int sig_number) {
     struct pcap_stat stats;
 
-    if (pcap_stats(pd, &stats) >= 0) {
+    if (handle && pcap_stats(handle, &stats) >= 0) {
         printf("\n%d packets captured\n", packets);
         printf("%d packets received\n", stats.ps_recv);
         printf("%d packets dropped\n\n", stats.ps_drop);
     }
 
-    pcap_close(pd);
+    if (handle) {
+        pcap_close(handle);
+    }
     exit(EXIT_SUCCESS);
 }
 
 void packet_handler(u_char *user, const struct pcap_pkthdr *packethdr, const u_char *packetptr) {
-    struct *ip iphdr;
-    struct *icmp icmphdr;
-    struct *tcphdr tcphdr;
-    struct *udphdr udphdr;
+    struct ip *iphdr;
+    struct icmp *icmphdr;
+    struct tcphdr *tcphdr;
+    struct udphdr *udphdr;
 
-    char ipdhrInfo[256];
+    char iphdrInfo[256];
     char srcip[256];
     char dstip[256];
 
     packetptr += linkhdrlen;
     iphdr = (struct ip*)packetptr;
-    strcpy(srcip, inet_ntoa(iphdr->ip_src));
-    strcpy(dstip, inet_ntoa(iphdr->ip_dst));
-    sprintf(iphdrInfo, "ID:%d TOS:0x%x, TTL:%d IpLen:%d DgLen:%d",
+
+    if (iphdr->ip_v != 4) {
+        printf("Not an IPv4 packet. Skipping...\n\n");
+        return;
+    }
+
+    inet_ntop(AF_INET, &(iphdr->ip_src), srcip, sizeof(srcip));
+    inet_ntop(AF_INET, &(iphdr->ip_dst), dstip, sizeof(dstip));
+
+    snprintf(iphdrInfo, sizeof(iphdrInfo), "ID:%d TOS:0x%x, TTL:%d IpLen:%d DgLen:%d",
             ntohs(iphdr->ip_id), iphdr->ip_tos, iphdr->ip_ttl,
             4*iphdr->ip_hl, ntohs(iphdr->ip_len));
-    
+
     packetptr += 4*iphdr->ip_hl;
+
     switch (iphdr->ip_p) {
         case IPPROTO_TCP:
             tcphdr = (struct tcphdr*)packetptr;
@@ -59,11 +69,9 @@ void packet_handler(u_char *user, const struct pcap_pkthdr *packethdr, const u_c
                    (tcphdr->th_flags & TH_PUSH ? 'P' : '*'),
                    (tcphdr->th_flags & TH_RST ? 'R' : '*'),
                    (tcphdr->th_flags & TH_SYN ? 'S' : '*'),
-                   (tcphdr->th_flags & TH_SYN ? 'F' : '*'),
+                   (tcphdr->th_flags & TH_FIN ? 'F' : '*'),  // Fixed: Was checking SYN instead of FIN
                    ntohl(tcphdr->th_seq), ntohl(tcphdr->th_ack),
                    ntohs(tcphdr->th_win), 4*tcphdr->th_off);
-            printf("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n\n");
-            packets += 1;
             break;
 
         case IPPROTO_UDP:
@@ -71,66 +79,73 @@ void packet_handler(u_char *user, const struct pcap_pkthdr *packethdr, const u_c
             printf("UDP  %s:%d -> %s:%d\n", srcip, ntohs(udphdr->uh_sport),
                    dstip, ntohs(udphdr->uh_dport));
             printf("%s\n", iphdrInfo);
-            printf("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n\n");
-            packets += 1;
             break;
- 
+
         case IPPROTO_ICMP:
             icmphdr = (struct icmp*)packetptr;
             printf("ICMP %s -> %s\n", srcip, dstip);
             printf("%s\n", iphdrInfo);
             printf("Type:%d Code:%d ID:%d Seq:%d\n", icmphdr->icmp_type, icmphdr->icmp_code,
-                   ntohs(icmphdr->icmp_hun.ih_idseq.icd_id), ntohs(icmphdr->icmp_hun.ih_idseq.icd_seq));
-            printf("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n\n");
-            packets += 1;
+                   ntohs(icmphdr->icmp_hun.ih_idseq.icd_id),
+                   ntohs(icmphdr->icmp_hun.ih_idseq.icd_seq));
             break;
-        }
+
+        default:
+            printf("Unknown protocol: %d\n", iphdr->ip_p);
+            break;
     }
+
+    printf("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n\n");
+    packets++;
+}
 
 int get_link_header_len(pcap_t *handle) {
     int linktype;
 
-    if ((linktype == pcap_datalink(handle)) == PCAP_ERROR) {
-        fprintf(stderr, "pcap_datalink(): ", pcap_geterr(handle));
-        return; 
+    if ((linktype = pcap_datalink(handle)) == PCAP_ERROR) {
+        fprintf(stderr, "pcap_datalink(): %s\n", pcap_geterr(handle));
+        return 0;
     }
-    
+
     switch (linktype) {
         case DLT_NULL:
-            linkhdrlen = 4;
-            break;
+            return 4;
         case DLT_EN10MB:
-            linkhdrlen = 14;
-            break;
+            return 14;
         case DLT_SLIP:
         case DLT_PPP:
-            linkhdrlen = 24;
-            break;
+            return 24;
         default:
             printf("Unsupported data link (%d)\n", linktype);
-            linkhdrlen = 0;
+            return 0;
     }
 }
 
 pcap_t* create_pcap_handle(char* device, char* filter) {
     char errbuf[PCAP_ERRBUF_SIZE];
-    pcap_t *handle = NULL;
     pcap_if_t *devices = NULL;
     struct bpf_program bpf;
     bpf_u_int32 netmask;
     bpf_u_int32 srcip;
 
-    if (!*device) {
-        if (pcap_findalldevs(&devices, errbuf)) {
+    if (!device[0]) {
+        if (pcap_findalldevs(&devices, errbuf) == PCAP_ERROR) {
             fprintf(stderr, "pcap_findalldevs(): %s\n", errbuf);
             return NULL;
         }
-        strcpy(device, devices[0].name);
+        if (devices == NULL) {
+            fprintf(stderr, "No devices found\n");
+            return NULL;
+        }
+        strncpy(device, devices[0].name, 255);
+        device[255] = '\0';
+        pcap_freealldevs(devices);
     }
 
     if (pcap_lookupnet(device, &srcip, &netmask, errbuf) == PCAP_ERROR) {
         fprintf(stderr, "pcap_lookupnet(): %s\n", errbuf);
-        return NULL;
+        netmask = 0;
+        srcip = 0;
     }
 
     handle = pcap_open_live(device, BUFSIZ, 1, 1000, errbuf);
@@ -139,46 +154,53 @@ pcap_t* create_pcap_handle(char* device, char* filter) {
         return NULL;
     }
 
-    if (pcap_compile(handle, &bpf, filter, 0, netmask) == PCAP_ERROR) {
-        fprintf(stderr, "pcap_compile(): %s\n", pcap_geterr(handle));
-        return NULL;
-    }
+    if (filter[0] != '\0') {
+        if (pcap_compile(handle, &bpf, filter, 0, netmask) == PCAP_ERROR) {
+            fprintf(stderr, "pcap_compile(): %s\n", pcap_geterr(handle));
+            pcap_close(handle);
+            return NULL;
+        }
 
-    if (pcap_setfilter(handle, &bpf) == PCAP_ERROR) {
-        fprintf(stderr, "pcap_setfilter(): %s\n", pcap_geterr(handle));
-        return NULL;
+        if (pcap_setfilter(handle, &bpf) == PCAP_ERROR) {
+            fprintf(stderr, "pcap_setfilter(): %s\n", pcap_geterr(handle));
+            pcap_close(handle);
+            return NULL;
+        }
+
+        pcap_freecode(&bpf);
     }
 
     return handle;
 }
 
 int main(int argc, char* argv[]) {
-    char device[256];
-    char filter[256];
+    char device[256] = {0};
+    char filter[256] = {0};
     int count = 0;
     int opt;
 
-    *device = 0;
-    *filter = 0;
-
-    while ((opt = getopt(argc, argv, "hi:n:") != -1)) {
+    while ((opt = getopt(argc, argv, "hi:n:")) != -1) {
         switch (opt) {
             case 'h':
-                printf("you need some help? waawaa go cry about it %s", argv[0]);
-                EXIT_SUCCESS;
-                break;
+                printf("Usage: %s [-i interface] [-n count] [filter]\n", argv[0]);
+                return EXIT_SUCCESS;
             case 'i':
-                strcpy(device, optarg);
+                strncpy(device, optarg, sizeof(device) - 1);
                 break;
             case 'n':
                 count = atoi(optarg);
                 break;
+            default:
+                fprintf(stderr, "Invalid option\n");
+                return EXIT_FAILURE;
         }
     }
-    
+
     for (int i = optind; i < argc; i++) {
-        strcat(filter, argv[i]);
-        strcat(filter, " ");
+        if (strlen(filter) + strlen(argv[i]) + 2 <= sizeof(filter)) {
+            strcat(filter, argv[i]);
+            strcat(filter, " ");
+        }
     }
 
     signal(SIGINT, stop_capture);
@@ -192,14 +214,16 @@ int main(int argc, char* argv[]) {
 
     linkhdrlen = get_link_header_len(handle);
     if (linkhdrlen == 0) {
+        pcap_close(handle);
         return EXIT_FAILURE;
     }
-    
+
     if (pcap_loop(handle, count, packet_handler, (u_char*)NULL) < 0) {
-        fprintf(stderr, "pcap_loop failed wawa: %s\n", pcap_geterr(handle));
+        fprintf(stderr, "pcap_loop failed: %s\n", pcap_geterr(handle));
+        pcap_close(handle);
         return EXIT_FAILURE;
     }
 
     stop_capture(0);
+    return EXIT_SUCCESS;
 }
-
