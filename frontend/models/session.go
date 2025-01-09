9eff485052a35b2d5558b15430eb63ad78cd0d8a
mkurzych <marta.kurzych@wp.pl>
2025-01-09 20:22:33 +0100

    feat: session representation

--- frontend/models/session.go
+++ frontend/models/session.go
@@ -1,1 +1,15 @@
 package models
+
+import (
+	"time"
+)
+
+// Session is a struct that represents a session
+type Session struct {
+	collectedPackets []Packet
+	filter           Filter
+	startTime        time.Time
+	endTime          time.Time
+	file             string
+	ports            []string
+}
