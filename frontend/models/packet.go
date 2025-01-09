78d3b492489fd1dc40719399bd7447d8a7085248
mkurzych <marta.kurzych@wp.pl>
2025-01-09 19:03:43 +0100

    feat: packet representation

--- frontend/models/packet.go
+++ frontend/models/packet.go
@@ -1,1 +1,34 @@
 package models
+
+import "time"
+
+// DataLink is a struct that represents the data link layer of a packet
+type DataLink struct {
+	SourceMAC      string
+	DestinationMAC string
+	Protocol       string
+}
+
+// Network is a struct that represents the network layer of a packet
+type Network struct {
+	SourceIP      string
+	DestinationIP string
+	Protocol      string
+}
+
+// Transport is a struct that represents the transport layer of a packet
+type Transport struct {
+	SourcePort      string
+	DestinationPort string
+	Protocol        string
+}
+
+// Packet is a struct that represents a packet
+type Packet struct {
+	ID        int
+	DataLink  DataLink
+	Network   Network
+	Transport Transport
+	TTL       int
+	datetime  time.Time
+}
