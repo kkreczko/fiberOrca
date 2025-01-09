222fecbd2eb64f3cd16271ce16afe50a8aaae568
mkurzych <marta.kurzych@wp.pl>
2025-01-09 20:20:52 +0100

    feat: filter representation

--- frontend/models/filter.go
+++ frontend/models/filter.go
@@ -1,1 +1,16 @@
 package models
+
+import "time"
+
+type Filter struct {
+	MAC                 string
+	IP                  string
+	senderPort          string
+	receiverPort        string
+	dataLinkProtocol    string
+	networkProtocol     string
+	transportProtocol   string
+	applicationProtocol string
+	startTime           time.Time
+	endTime             time.Time
+}
