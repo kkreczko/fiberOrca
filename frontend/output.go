c9a1ac23bef66cfe876c89a01c34991ecd7fb2d1
Konrad Kreczko <konradkreczko@gmail.com>
2025-01-29 20:28:31 +0100

    chore: Reformat code for readability

--- frontend/output.go
+++ frontend/output.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
 // TEMPORARY OUTPUT FUNCTION IT WILL BE CHANGED FOR BUBBLETEA INTEGRATION PURPOSES
 // FOR NOW IT WORKS
 func outputData(packet *Packet) {
-    if packet == nil {
-        return
-    }
+	if packet == nil {
+		return
+	}
 
 	fmt.Printf("Protocol: %s\n", packet.Protocol)
 	fmt.Printf("Source: %s:%d\n", packet.SourceIP, packet.SourcePort)
