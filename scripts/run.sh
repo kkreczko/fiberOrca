5738dad4eb521a889d1f1f8baf8bd58201f65136
Konrad Kreczko <konradkreczko@gmail.com>
2025-01-30 19:35:31 +0100

    feat: Add install_libpcap.sh and run.sh scripts for temporary usage

---
+++ scripts/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then 
+    echo "You need root privileges for this program to work"
+    exec sudo "$0" "$@"
+    exit
+fi
+
+FRONTEND="./../release/fiberOrca"
+BACKEND="./../release/network_analyzer"
+
+$FRONTEND &
+FRONTEND_PID=$!
+sleep 2
+$BACKEND
+kill $FRONTEND_PID
