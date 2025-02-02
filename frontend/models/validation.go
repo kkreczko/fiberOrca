dac59858a3089253cee454c4bd9bd9f02eb75a1a
dorbenn <dorbenn@dorbenn-laptop.com>
2025-02-02 19:22:14 +0100

    fix: fixed validation bug

--- frontend/models/validation.go
+++ frontend/models/validation.go
@@ -7,7 +7,7 @@ import (
 
 func ValidateIP(IPString string) error {
 	match, _ := regexp.MatchString("^((25[0-5]|(2[0-4]|1\\d|[1-9]|)\\d)\\.?\\b){4}$", IPString)
-	if !match {
+	if !match && IPString != "" {
 		return fmt.Errorf("invalid IP address")
 	}
 	return nil
@@ -15,7 +15,7 @@ func ValidateIP(IPString string) error {
 
 func ValidatePort(portString string) error {
 	match, _ := regexp.MatchString("^[0-9]{1,4}$", portString)
-	if !match {
+	if !match && portString != "" {
 		return fmt.Errorf("invalid port")
 	}
 	return nil
@@ -23,7 +23,7 @@ func ValidatePort(portString string) error {
 
 func ValidateTime(timeString string) error {
 	match, _ := regexp.MatchString("^(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}) $", timeString)
-	if !match {
+	if !match && timeString != "" {
 		return fmt.Errorf("invalid time")
 	}
 	return nil
