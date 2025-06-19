3f710213d86b1da9a7f559ca99949b0346a5df79
Konrad Kreczko <konradkreczko@gmail.com>
2025-06-19 23:18:20 +0200

    refactor: Updated frontend socket connection code

--- frontend/connect.go
+++ frontend/connect.go
@@ -8,12 +8,8 @@ import (
 	"os"
 )
 
-// IT MIGHT BE ADDED SOMEWHERE AS ENVIRONMENTAL VARIABLE
 const SOCKET_PATH = "/tmp/fiber_orcas.sock"
 
-// THIS FUNCTION CONNECTS TO CERTAIN UNIX SOCKET AND ACCEPTS ANY INCOMING MESSAGES
-// THEN IT CALLBACKS OUR HANDLER FUNCTION THAT READS PACKETS FROM TEMPORARY BUFFER AND DOES SOMETHING WITH THEM
-// 1. CONNECT -> 2. READ -> 3. PARSE -> 4. OUTPUT -> REPEAT 2
 func connectToSocket(s *models.Session, ch chan string) {
 	if err := os.Remove(SOCKET_PATH); err != nil && !os.IsNotExist(err) {
 		log.Fatal(err)
@@ -36,11 +32,6 @@ func connectToSocket(s *models.Session, ch chan string) {
 	}
 }
 
-// FUNCTION THAT GETS PACKETS FROM SOCKET AND DOES SOMETHING WITH THEM
-// NO RESEARCH WAS DONE WHETHER IT IS THE MOST MEMORY EFFICIENT IMPLEMENTATION
-// NO RESEARCH WAS DONE WHETHER ANY PACKETS ARE LOST
-// IT CAUGHT IMPORTANT ONES DURING VERY EXTENSIVE :rofl: TESTING
-// IT JUST WORKS
 func handleConnection(conn net.Conn, s *models.Session, ch chan string) {
 	defer conn.Close()
 
