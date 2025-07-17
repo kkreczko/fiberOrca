e30661818c455a7df9202814a7ed1f839b00f924
Konrad Kreczko <konradkreczko@gmail.com>
2025-07-17 23:35:12 +0200

    Added build net to makefile, playing with raw sockets

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
 clean:
 	rm ./release/*
+
+build-net:
+	@mkdir -p output
+	@clang -I=./net/include/ ./net/main.cpp -o ./output/net-bin
--- net/main.cpp
+++ net/main.cpp
@@ -1,6 +1,9 @@
-
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
 
 int main()
 {
+  int raw_socket = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
   return 0;
 }
